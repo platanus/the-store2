0c419354221df6d5655303b3e1b67f79e8216543
Diego Fernández <difernandez@uc.cl>
2022-12-13 10:51:11 -0300

    refactor(): remove example vue component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue';
-import App from './components/app.vue';
 import TheHeader from './components/the-header.vue';
 import './css/application.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = createApp({
     components: {
-      App,
       TheHeader,
     },
   });
