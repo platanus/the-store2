49ea93a571f72c31508a31fae6e89e98dedb8eee
Diego Fernández <difernandez@uc.cl>
2022-12-13 10:49:50 -0300

    feat(application-layout): add header

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue';
 import App from './components/app.vue';
+import TheHeader from './components/the-header.vue';
 import './css/application.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = createApp({
-    components: { App },
+    components: {
+      App,
+      TheHeader,
+    },
   });
   app.mount('#vue-app');
 
---
+++ app/javascript/components/the-header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="flex w-full items-center bg-blue-800 px-8 py-2 text-white">
+    <h1 class="text-2xl font-bold">
+      The Store
+    </h1>
+  </header>
+</template>
