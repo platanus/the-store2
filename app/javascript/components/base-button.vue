3cd608551e4bfe9fa5d40e90c457a869a3b4fff9
Diego Fernández <difernandez@uc.cl>
2022-12-13 12:57:37 -0300

    feat(components): add base form components

---
+++ app/javascript/components/base-button.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const variants = {
+  primary: 'bg-blue-800 text-white hover:bg-blue-900',
+  secondary: 'border-blue-800 bg-transparent text-blue-800 hover:bg-blue-50',
+  danger: 'bg-rose-400 hover:bg-rose-500 text-white',
+  success: 'bg-green-400 text-white hover:bg-green-500',
+  disabled: 'bg-zinc-500 text-white cursor-not-allowed',
+};
+
+interface Props {
+  variant?: keyof typeof variants
+}
+withDefaults(
+  defineProps<Props>(),
+  {
+    variant: 'primary',
+  },
+);
+</script>
+
+<template>
+  <button
+    class="rounded px-3 py-2 text-sm font-semibold"
+    :class="variants[variant]"
+  >
+    <slot />
+  </button>
+</template>
